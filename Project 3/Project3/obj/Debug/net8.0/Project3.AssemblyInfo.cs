531acb790cfb5f5e792516467b540436f26b2f0e
Omagar1 <josiah.rowden@gmail.com>
2025-01-06 17:31:09 +0000

    project 3 writeup tweek

--- Project 3/Project3/obj/Debug/net8.0/Project3.AssemblyInfo.cs
+++ Project 3/Project3/obj/Debug/net8.0/Project3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f854e73e613a80587b854657808d68028d85efa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb899627b3056c883e991f85c2dc03577f21a826")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
